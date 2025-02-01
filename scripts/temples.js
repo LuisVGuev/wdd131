437abccc870aa3bc6437d3c2a7a8f396b4148c9f
LuisVGuev <tag.interaction.web@gmail.com>
2025-01-31 23:31:32 -0300

    main

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,9 +2,9 @@
 document.getElementById('currentyear').textContent = new Date().getFullYear();
 document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
 
-//Menu
+
 document.getElementById("menu").addEventListener("click", function() {
-  let nav = document.querySelector("nav ul");
-  nav.classList.toggle("visible"); // Add the 'visible' class to toggle visibility
+ let nav = document.querySelector("nav ul");
+ nav.classList.toggle("active"); 
 });
 
