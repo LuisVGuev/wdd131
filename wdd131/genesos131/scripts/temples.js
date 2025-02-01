742987b869394c1141cb26bdd2c11d6554a6c060
LuisVGuev <tag.interaction.web@gmail.com>
2025-02-01 00:05:19 -0300

    main

--- wdd131/genesos131/scripts/temples.js
+++
@@ -1,9 +0,0 @@
-//Year anda las modified
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
-
-//Menu
-document.getElementById("menu").addEventListener("click", function() {
-  let nav = document.querySelector("nav ul");
-  nav.classList.toggle("visible"); // Add the 'visible' class to toggle visibility
-});
